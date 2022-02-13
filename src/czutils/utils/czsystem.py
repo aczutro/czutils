8b54e8bc2244f8afb8992903fa8dc0cec72caf12
Alexander Czutro <github@czutro.ch>
2022-02-13 22:54:28 +0100

    czsystem: new function resolveAbsPath

--- src/czutils/utils/czsystem.py
+++ src/czutils/utils/czsystem.py
@@ -12,8 +12,32 @@
 
 """Help functions related to the (operating) system."""
 
+from .czlogging import *
+import os
 import os.path
 import sys
+
+
+_logger = LoggingChannel("czutils.utils.czsystem",
+                         LoggingLevel.SILENT,
+                         colour=True)
+
+def setLoggingLevel(level: int, colour = True) -> None:
+    """
+    Sets this module's logging level.  If not called, the logging level is
+    SILENT.
+
+    :param level: One of the following:
+                  - czlogging.LoggingLevel.INFO
+                  - czlogging.LoggingLevel.WARNING
+                  - czlogging.LoggingLevel.ERROR
+                  - czlogging.LoggingLevel.SILENT
+
+    :param colour: If true, use colour in log headers.
+    """
+    global _logger
+    _logger = LoggingChannel("czutils.utils.czsystem", level, colour=colour)
+#setLoggingLevel
 
 
 def appName() -> str:
@@ -37,6 +61,32 @@ def isProperDir(directory: str):
     """
     return os.path.isdir(directory) and not os.path.islink(directory)
 #_isProperDir
+
+
+def resolveAbsPath(inputPath: str) -> str:
+    """
+    Returns an absolute path version of inputPath.
+    If inputPath is an absolute path, returns inputPath.
+    Else, interprets inputPath relative to ${HOME}.
+    If the HOME environment variable is not defined, interprets inputPath
+    relative to the current working directory.
+    """
+    inputPath = os.path.normpath(inputPath)
+
+    if inputPath == os.path.abspath(inputPath):
+        return inputPath
+    #if
+
+    try:
+        home = os.environ["HOME"]
+    except KeyError:
+        _logger.warning("environment variable HOME not defined")
+        home = os.path.abspath(os.path.curdir)
+    #except
+
+    return os.path.join(home, inputPath)
+
+#resolveFileLocation
 
 
 ### aczutro ###################################################################
